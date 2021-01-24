b2d462576274e37d2de8b4e9e8856d7c19de6643
bweinand <bernd@weinand.eu>
2021-01-24 19:16:59 +0100

    1 file modified

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <hr/>
+    ~~~~~~~ BW ~~~~~~~
+    <hr>
+
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
